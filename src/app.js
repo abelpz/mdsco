4828ba51f6ba86b2fcabab564df9aaa976afa805
Abel <abelper54@gmail.com>
2021-04-04 17:17:48 -0400

    added reveal and markdown

--- src/app.js
+++ src/app.js
@@ -2,8 +2,22 @@ import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js'
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/black.css'
+import './scorm_api_wrapper.js'
 
 let deck = new Reveal({
     plugins: [ Markdown ]
  })
  deck.initialize();
+
+ Reveal.addEventListener( 'slidechanged', function( event ) {
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+
+    pipwerks.SCORM.set("cmi.core.lesson_location", event.currentSlide);
+    pipwerks.SCORM.save();
+
+    if(event.currentSlide === Reveal.getTotalSlides()){
+        pipwerks.SCORM.set("cmi.core.lesson_status", "completed");
+        pipwerks.SCORM.quit();
+    }
+
+} );
---
+++ src/scorm-generator.js
@@ -0,0 +1,1 @@
+const manifest = '../dist/imsmanifest.xml'
